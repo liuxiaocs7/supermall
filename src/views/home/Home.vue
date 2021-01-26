9e245fc0a53d64c56463ed8344ddf03a8f9637d0
liuxiao <liuxiao2103@qq.com>
2021-01-26 11:43:16 +0800

    增加主页导航

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,19 @@
 <template>
-  <h2>主页</h2>
+  <div id="home">
+    <nav-bar>
+
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+
 export default {
   name: 'Home',
+  components: {
+    NavBar
+  }
 }
 </script>
 
